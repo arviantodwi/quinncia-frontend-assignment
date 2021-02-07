43efb0eebfe883d97aa2623003e1b366cd3087fe
Arvianto Dwi <arvianto.dwi@gmail.com>
2021-02-07 18:15:23 +0700

    🤔️ Quick implementation of adding class to html tag

--- src/client/redux/modal.js
+++ src/client/redux/modal.js
@@ -23,8 +23,10 @@ export const setModalContentType = contentType => ({
 const modalReducer = (state = defaultState, action) => {
   switch (action.type) {
     case MODAL_SHOW:
+      document.getElementsByTagName('html')[0].classList.add('is-clipped');
       return Object.assign({}, state, { show: true });
     case MODAL_CLOSE:
+      document.getElementsByTagName('html')[0].classList.remove('is-clipped');
       return Object.assign({}, state, defaultState);
     case MODAL_CONTENT_TYPE:
       return Object.assign({}, state, action.payload);
